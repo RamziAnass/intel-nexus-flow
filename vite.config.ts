9c6a3bad20c4dc200bdb27853dde0dda5e62b04a
Ramzi Anass <kleyger11@gmail.com>
2025-05-24 16:59:56 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 // vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
 
-export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: "::",
+    port: 8080,
+  },
+   plugins: [react()],
   base: '/intel-nexus-flow/', // Set this to your repository name
-});
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
